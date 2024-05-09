b2fe176e43ba6acc6ced299b78b836c14820d9e1
TimiAndrei <timiandrei223@gmail.com>
2024-05-09 20:23:39 +0300

    added custom EmailException

--- src/main/java/com/timi/dao/UserDAO.java
+++ src/main/java/com/timi/dao/UserDAO.java
@@ -1,6 +1,7 @@
 package com.timi.dao;
 
 import com.timi.exception.DAOException;
+import com.timi.exception.InvalidEmailException;
 import com.timi.model.User;
 import java.util.List;
 
@@ -9,6 +10,6 @@ public interface UserDAO {
     void addUser(User user) throws DAOException;
     User getUserById(int id) throws DAOException;
     List<User> getAllUsers() throws DAOException;
-    void updateUser(User user) throws DAOException;
+    void updateUser(User user) throws DAOException, InvalidEmailException;
     void deleteUser(int id) throws DAOException;
 }
